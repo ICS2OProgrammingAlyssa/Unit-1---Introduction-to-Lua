3ac9533af7341371993250f1c55cfb84a8896ea2
Alyssa Bleckwenn <61517084+s466881@users.noreply.github.com>
2020-03-09 16:06:02 -0400

    shsdxhnjdtehdge

--- AreaRectCircleAlyssa/main.lua
+++ AreaRectCircleAlyssa/main.lua
@@ -7,12 +7,18 @@
 -- Your code here
 
 -- create my local variables
-local areaText
-local textSize = 50
+local areaTextCircle
+local textSize = 45
 local myCircle
 local PI = 3.14
 local radiusOfCircle = 169
 local areaOfCircle
+local myRectangle
+local areaTextRectangle
+local widthOfRectangle = 300
+local heightOfRectangle = 269
+local areaOfRectangle
+
 
 -- set the background color of my screen. colors are between 0 and 1
 display.setDefault("background", 240/255, 255/255, 240/255)
@@ -42,15 +48,49 @@ myCircle:setStrokeColor(0.3, 0.3, 0.3)
 areaOfCircle = radiusOfCircle * radiusOfCircle * PI
 
 -- write the text on the screen
-areaText = display.newText("The area of this circle with a radius of \n" ..
+areaTextCircle = display.newText("The area of the circle with a radius of \n" ..
 	radiusOfCircle .. " is " ..
-	areaOfCircle .. "pixels squared.",0, 0, Arial, textSize)
+	areaOfCircle .. "pixels squared.", 0, 0, Arial, textSize)
 
 -- anchor the text and set its (x,y) position.
-areaText.anchorY = 0
-areaText.anchorX = 0
-areaText.x = 20
-areaText.y = display.contentHeight/2
+areaTextCircle.anchorY = 0
+areaTextCircle.anchorX = 0
+areaTextCircle.x = 20
+areaTextCircle.y = display.contentHeight/2
 
 -- set the color of the new text
-areaText:setTextColor(0, 0, 0)
+areaTextCircle:setTextColor(0, 0, 0)
+
+-- draw the rectangle 
+myRectangle = display.newRect(0, 0, widthOfRectangle, heightOfRectangle)
+
+-- anchor the rectangle 
+myRectangle.anchorX = 0
+myRectangle.anchorY = 0
+myRectangle.x = 550
+myRectangle.y = 50
+
+-- set the width of the border
+myRectangle.strokeWidth = 14
+
+-- set the color of the rectangle
+myRectangle:setFillColor(175/225, 147/225, 199/255)
+
+-- set the color of the boerder
+myRectangle:setStrokeColor(174/255, 174/255, 174/255)
+
+-- calculate the area
+areaOfRectangle = widthOfRectangle * heightOfRectangle
+
+-- write the area on the screen. 
+areaTextRectangle = display.newText("The area of the rectangle with a width of \n" ..
+	widthOfRectangle .. " and a height of " .. heightOfRectangle .. " is " ..
+	areaOfRectangle .. " pixels squared.", 0, 0, Arial, textSize)
+
+-- anchor the text and set its (x,y) posistion
+areaTextRectangle.anchorX = 0
+areaTextRectangle.anchorY = 0
+areaTextRectangle.x = 20
+areaTextRectangle.y = 500
+-- set the color of the text
+areaTextRectangle:setTextColor(0, 0, 0)
