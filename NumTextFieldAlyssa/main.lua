be27c5dbafc4801f32c29a4da17e0a5df35a6f51
Alyssa Bleckwenn <alyssa.bleckwenn@ocsbstudent.ca>
2020-03-10 14:31:43 -0400

    worked on num text field

--- NumTextFieldAlyssa/main.lua
+++ NumTextFieldAlyssa/main.lua
@@ -1,7 +1,97 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: NumTextField
+-- Name: Alyssa
+-- Course: ISC2O
+-- Description: this program asks the user an addition question and displays "correct" if the
+-- answer is correct, and displays "incorrect" if the answer is incorrect.
+------------------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set the background color
+display.setDefault("background", 239/255, 239/255, 239/255)
+
+---------------------------------------------------------------------------------------------------
+--LOCAL VARIABLES
+---------------------------------------------------------------------------------------------------
+
+--create local variables
+local questionObject
+local correctObject
+local incorrectObject
+local numericField
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+
+-------------------------------------------------------------------------------------------------
+-- LOCAL FUNCIONS
+-------------------------------------------------------------------------------------------------
+
+local function AskQuestion()
+	-- generate 2 random numbers between a max. and a min. number
+	randomNumber1 = math.ramdom(0, 8)
+	randomNumber2 = math.random(0, 8)
+
+	correctAnswer = randomNumber1 + randomNumber2
+
+	-- create question in text object
+	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+end
+
+local function HideCorrect()
+	correctObject.isVisible = false
+	AskQuestion()
+end
+
+local function HideIncorrect()
+	incorrectObject.isVisible = false
+	AskQuestion()
+end
+
+local function NumericFieldListener( event )
+
+	-- user begins editing "numericField"
+	if ( event.phase == "began" ) then
+
+		-- clear text field
+		event.target.text = ""
+
+	elseif ( event.phase == "submitted" ) then
+
+		-- when the answer is submitted (enter key is pressed) set user input to users answer
+		userAnswer = tonumber(event.target.text)
+
+		--if the users answer and the correct answer are the same:
+		if (userAnswer == correctAnswer) then
+			correctObject.isVisible = true
+			timer.performWithDelay(1500, HideCorrect)
+
+		else incorrectObject.isVisible = true
+			timer.performWithDelay(1500, HideIncorrect)
+
+	end
+end
+
+----------------------------------------------------------------------------------------------
+-- OBJECT CREATION	
+----------------------------------------------------------------------------------------------
+
+-- displays a question and sets the color
+questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+questionObject:setTextColor( 20/255, 113/255, 235/255 )
+
+-- create the correct answer text object and make it invisible
+correctObject = display.newText( "Correct!! :)", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+correctObject:setTextColor(15/255, 243/255, 45/255)
+correctObject.isVisible = false
+
+incorrectObject = display.newText( "Incorrect. :(", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+incorrectObject:setTextColor(245/255, 10/255, 10/255)
+incorrectObject.isVisible = false
+
+-- create a numeric text field
+numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80)
+numericField.inputType = "number"
